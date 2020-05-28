d809e4e142e78fd79082c938c4589b2130ee80ae
Mohammad Ahmadi <47987499+MrMohammadAhmadi@users.noreply.github.com>
2020-05-28 20:35:09 +0430

    Updates

---
+++ precache-manifest.ecad3b49ae793396f134f65b0d7c3598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631c6359bd7fac8c0b4534d9c1fbd3b7",
+    "url": "/todolist-app-demo/index.html"
+  },
+  {
+    "revision": "6ee92d9d4dc3dd72a141",
+    "url": "/todolist-app-demo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1942f2d213c9882a67e3",
+    "url": "/todolist-app-demo/static/css/main.b9653fcd.chunk.css"
+  },
+  {
+    "revision": "6ee92d9d4dc3dd72a141",
+    "url": "/todolist-app-demo/static/js/2.480bc55d.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/todolist-app-demo/static/js/2.480bc55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1942f2d213c9882a67e3",
+    "url": "/todolist-app-demo/static/js/main.4d3a928e.chunk.js"
+  },
+  {
+    "revision": "ecb0c05bd1678dabb205",
+    "url": "/todolist-app-demo/static/js/runtime-main.f243189a.js"
+  }
+]);
